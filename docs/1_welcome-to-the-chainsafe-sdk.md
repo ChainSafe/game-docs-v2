137f836eb72f346d7eec6a94545653fe62b51fa8
sneakzttv <57473220+sneakzttv@users.noreply.github.com>
2023-01-24 01:28:02 +0800

    current sdk moved above
    current sdk moved above

--- docs/1_welcome-to-the-chainsafe-sdk.md
+++ docs/1_welcome-to-the-chainsafe-sdk.md
@@ -10,10 +10,10 @@ SDK: [https://github.com/ChainSafe/web3.unity/releases](https://github.com/Chain
 
 Bridging Unity games to the blockchain. Create your in game NFTs.
 
-## Legacy Builds 1.6
-
-Press here: [https://docs.gaming.chainsafe.io/v1/getting-started-v1](https://docs.gaming.chainsafe.io/v1/getting-started-v1) to open our legacy documentation. (use the left sidebar when in local testing as these links are not active yet)
-
 ## Current SDK Builds
 
 Press here: [https://docs.gaming.chainsafe.io/v2/version-2-integration](https://docs.gaming.chainsafe.io/v2/version-2-integration) to open our current SDK documentation. (use the left sidebar when in local testing as these links are not active yet)
+
+## Legacy Builds 1.6
+
+Press here: [https://docs.gaming.chainsafe.io/v1/getting-started-v1](https://docs.gaming.chainsafe.io/v1/getting-started-v1) to open our legacy documentation. (use the left sidebar when in local testing as these links are not active yet)
--- sidebars.js
+++ sidebars.js
@@ -10,13 +10,13 @@ module.exports = {
         'welcome-to-the-chainsafe-sdk',
         {
           type: 'category',
-          label: 'V1 Legacy Documentation',
-          items: ['v1/getting-started-v1', 'v1/tutorial', 'v1/installation', 'v1/project-id-registration', 'v1/evm-blockchain', 'v1/erc1155', 'v1/erc721', 'v1/erc20', 'v1/custom-interactions', 'v1/minter-evm', 'v1/in-game-marketplace', 'v1/minting-with-voucher', 'v1/webgl', 'v1/mobile-and-desktop', 'v1/keep-your-games-safe-from-cheaters', 'v1/importing-nfts', 'v1/implementing-social-logins-to-webgl-builds', 'v1/how-tos', 'v1/media', 'v1/faq',],
+          label: 'SDK Documentation',
+          items: ['v2/getting-started', 'v2/installation', 'v2/project-id-registration', 'v2/erc1155-interactions', 'v2/erc721-interactions', 'v2/erc20-interactions', 'v2/using-the-minter', 'v2/in-game-marketplace', 'v2/minting-with-voucher', 'v2/webgl-browser', 'v2/web3wallet-mobile-and-desktop', 'v2/keep-your-games-safe-from-cheaters', 'v2/importing-nfts', 'v2/implementing-social-logins-to-webgl-builds', 'v2/how-tos', 'v2/media', 'v2/faq',],
         },
         {
           type: 'category',
-          label: 'SDK Documentation',
-          items: ['v2/getting-started', 'v2/installation', 'v2/project-id-registration', 'v2/erc1155-interactions', 'v2/erc721-interactions', 'v2/erc20-interactions', 'v2/using-the-minter', 'v2/in-game-marketplace', 'v2/minting-with-voucher', 'v2/webgl-browser', 'v2/web3wallet-mobile-and-desktop', 'v2/keep-your-games-safe-from-cheaters', 'v2/importing-nfts', 'v2/implementing-social-logins-to-webgl-builds', 'v2/how-tos', 'v2/media', 'v2/faq',],
+          label: 'V1 Legacy Documentation',
+          items: ['v1/getting-started-v1', 'v1/tutorial', 'v1/installation', 'v1/project-id-registration', 'v1/evm-blockchain', 'v1/erc1155', 'v1/erc721', 'v1/erc20', 'v1/custom-interactions', 'v1/minter-evm', 'v1/in-game-marketplace', 'v1/minting-with-voucher', 'v1/webgl', 'v1/mobile-and-desktop', 'v1/keep-your-games-safe-from-cheaters', 'v1/importing-nfts', 'v1/implementing-social-logins-to-webgl-builds', 'v1/how-tos', 'v1/media', 'v1/faq',],
         },
       ],
     },
